db21faa4776fba3d90e325c58de1dca0f3a8a495
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-04-14 22:15:40 +0530

    Add: sigmoid-and-relu - submission-0

---
+++ Machine Learning/sigmoid-and-relu/submission-0.py
@@ -0,0 +1,17 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+    
+    def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array
+        # Formula: 1 / (1 + e^(-z))
+        # return np.round(your_answer, 5)
+        sig = 1 / (1 + np.exp(-z))
+        return np.round(sig, 5)
+
+    def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array
+        # Formula: max(0, z) element-wise
+        return np.where(z > 0, z, 0)
