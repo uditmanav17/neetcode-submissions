653c5a514a4abe0d95274b8fe90877a58c945316
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-06-18 14:03:56 +0530

    Add: sql-countries-you-can-safely-invest-in - submission-1

---
+++ SQL/sql-countries-you-can-safely-invest-in/submission-1.sql
@@ -0,0 +1,9 @@
+-- Write your query below
+
+SELECT
+    c.name AS country
+FROM person p
+JOIN country c ON SUBSTRING(p.phone_number, 1, 3) = c.country_code
+JOIN calls ca ON p.id = ca.caller_id OR p.id = ca.callee_id
+GROUP BY c.name
+HAVING AVG(ca.duration) > (SELECT AVG(duration) FROM calls)
