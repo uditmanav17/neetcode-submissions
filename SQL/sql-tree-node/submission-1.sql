640a45c886470f7d3e5fc1cebeed18ad0be6a18d
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-06-26 18:02:59 +0530

    Add: sql-tree-node - submission-1

---
+++ SQL/sql-tree-node/submission-1.sql
@@ -0,0 +1,9 @@
+-- Write your query below
+SELECT 
+    id,
+    CASE 
+        WHEN p_id IS NULL THEN 'Root'
+        WHEN id IN (SELECT p_id FROM tree WHERE p_id IS NOT NULL) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS type
+FROM tree
